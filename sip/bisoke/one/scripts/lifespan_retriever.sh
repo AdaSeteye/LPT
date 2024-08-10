30dea5538a962251e106a2b588ea8cf4638d604a
baimamboukar <baimamboukar@gmail.com>
2024-08-10 23:47:49 +0200

    chore: update lifespan

--- sip/bisoke/one/scripts/lifespan_retriever.sh
+++ sip/bisoke/one/scripts/lifespan_retriever.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 COUNTRY_ISO_CODE="$1"
 CSV_FILE="life-expectancy.csv"
 
@@ -10,11 +9,12 @@ fi
 
 # Search for the country ISO code in the CSV file and print the corresponding life expectancy
 lifespan=$(awk -F',' -v code="$COUNTRY_ISO_CODE" '$4 == code {print $NF}' "$CSV_FILE")
-
+#echo "Life expectancy for $COUNTRY_ISO_CODE: $lifespan"
 # Round the lifespan value to the nearest integer
 if [ -n "$lifespan" ]; then
+    # Round the lifespan to the nearest integer
     rounded_lifespan=$(printf "%.0f\n" "$lifespan")
-    echo "$lifespan"
+    echo "$rounded_lifespan"
 else
     echo "70"
 fi
